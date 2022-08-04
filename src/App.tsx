46768d7770689130f499c3ab3aa38f57988d4a25
Qhoirul Anwar <qhoir.anwar@gmail.com>
2022-08-04 10:58:52 +0700

    setup refine basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Refine } from "@pankod/refine-core";
+import {
+  Layout,
+  ReadyPage,
+  notificationProvider,
+  ErrorComponent,
+} from "@pankod/refine-antd";
+import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-simple-rest";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "@pankod/refine-antd/dist/styles.min.css";
 
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <Refine
+      routerProvider={routerProvider}
+      dataProvider={{
+        default: dataProvider("https://api.fake-rest.refine.dev"),
+        example: dataProvider("https://api.fake-rest.refine.dev")
+      }}
+      Layout={Layout}
+      ReadyPage={ReadyPage}
+      notificationProvider={notificationProvider}
+      catchAll={<ErrorComponent />}
+    />
+  );
+};
 
-export default App
+export default App;
